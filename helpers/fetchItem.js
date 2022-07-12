1fc5a44ef20c0f975766c724ea670320415aae4b
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-12 12:12:16 -0300

    Requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (productID) => {
+  const url = `https://api.mercadolibre.com/items/${productID}`;
+  if (url.endsWith('undefined')) return new Error('You must provide an url');
+
+  const promiseFetch = await fetch(url);
+  const results = await promiseFetch.json();
+
+  return results;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,8 +1,29 @@
-require('../mocks/fetchSimulator');
-const { fetchItem } = require('../helpers/fetchItem');
-const item = require('../mocks/item');
+require("../mocks/fetchSimulator");
+const { fetchItem } = require("../helpers/fetchItem");
+const item = require("../mocks/item");
 
-describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe("2 - Teste a função fetchItem", () => {
+  test("se fetchItens é uma função", () => {
+    expect.assertions(1);
+    expect(typeof fetchItem).toBe("function");
+  });
+  test("se a função fetchItem chama o fetch", () => {
+    expect.assertions(1);
+    fetchItem("MLB1615760527");
+    expect(fetch).toBeCalledTimes(1);
+  });
+  test("se a função fetchItem chama o endpoint", async () => {
+    expect.assertions(1);
+    const endpoint = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith(endpoint);
+  })
+  test('se a função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto `item` que já está importado no arquivo', async () => {
+    expect.assertions(1);
+    expect(await fetchItem('MLB1615760527')).toStrictEqual(item);
+  })
+  test('Se a função fetchItem chamada sem argumento retorna um erro', async () => {
+    expect.assertions(1);
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  })
 });
