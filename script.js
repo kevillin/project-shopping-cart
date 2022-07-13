c3c83cfb4224eed5f179a43740bb89cf3345868f
Kevillin Santos <kevillin_santos@hotmail.com>
2022-07-13 15:42:03 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
 const cartItemClickListener = (event) => {
-  // coloque seu código aqui s
+  const esvazia = document.querySelector('.cart__items');
+  esvazia.removeChild(event.target);
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
